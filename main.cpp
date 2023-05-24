ce8bf43a1f44d2a70070f7b41cd05d1d4fa02afb
romtec123 <julianlevin11@gmail.com>
2023-05-23 21:48:48 -0700

    Changes: Program now runs indefinitely- meant to be called by systemd Added coordinates struct, timer function, parseCoordinates function and calculateDistance function Now saves the last scan coordinates in memory and will return if a new scan is within ~50ft

--- main.cpp
+++ main.cpp
@@ -4,13 +4,25 @@
 #include <ctime>
 #include <fstream>
 #include <sstream>
+#include <chrono>
+#include <thread>
+#include <functional>
 #include "exec.h"
 
 using namespace std;
 
+struct Coordinates {
+    double latitude;
+    double longitude;
+};
 bool isExistingDir(const filesystem::path& p) noexcept;
-bool CreateDirectoryRecursive(const std::string & dirName);
-
+bool CreateDirectoryRecursive(const string & dirName);
+void timer_start(function<void(void)> func, unsigned int interval);
+double calculateDistance(double lat1, double lon1, double lat2, double lon2);
+Coordinates parseCoordinates(const string& inputString);
+void scan();
+string dataPath;
+string lastPos = "";
 
 
 int main() {
@@ -18,28 +30,54 @@ int main() {
 
     //Get home directory
     string homedir = getenv("HOME"); //stack overflow says this is bad but idk
-    if(homedir.empty())
-    {
+    if (homedir.empty()) {
         cout << "Error: Could not get home directory!" << endl;
         return 0;
     }
-    const string dataPath = homedir + "/wispy/raw/";
+    dataPath = homedir + "/wispy/raw/";
 
     //Create data directory
-    if(!isExistingDir(dataPath))
-    {
+    if (!isExistingDir(dataPath)) {
         CreateDirectoryRecursive(dataPath);
         cout << "Created directories: " << dataPath << endl;
     }
 
+    //Start timer
+    timer_start(scan, 20000);
+
+    while(true);
+
+
+}
+
+void scan() {
+
+    cout << "Starting scan..." << endl;
+
     //Get location
-    //This commands works for me, with my Pi. If you want to make it more portable, make a PR.
+    //These commands work for me, with my Pi. If you want to make it more portable, make a PR.
     string loc = execute("gpspipe -w -x 10 -n 10 | grep -m 1 TPV | jq -r '[.lat, .lon] | @csv'");
     if(loc.empty() || loc.size() < 5)
     {
-        cout << "Location was not found, exiting." << endl;
-        return 1;
+        cout << "Location was not found. Skipping..." << endl;
+        return;
     }
+
+    if(!lastPos.empty())
+    {
+        Coordinates last = parseCoordinates(lastPos);
+        Coordinates current = parseCoordinates(loc);
+
+        double distance = calculateDistance(last.latitude, last.longitude, current.latitude, current.longitude);
+
+        cout << "Scan Distance: " << distance << endl;
+        if(distance < 50)
+        {
+            cout << "To close to last scan pos! Skipping..." << endl;
+            return;
+        }
+    }
+    lastPos = loc;
 
 
     //Get iwlist output
@@ -47,13 +85,13 @@ int main() {
 
     if(iwList.empty() || iwList.size() < 16)
     {
-        cout << "Couldn't find any networks, exiting." << endl;
-        return 1;
+        cout << "Couldn't find any networks. Skipping..." << endl;
+        return;
     }
 
     //Generate unique name
-    auto t = std::time(nullptr);
-    auto tm = *std::localtime(&t);
+    auto t = time(nullptr);
+    auto tm = *localtime(&t);
     ostringstream oss;
     oss << put_time(&tm, "%d-%m-%Y_%H-%M-%S");
     auto time = oss.str();
@@ -70,7 +108,8 @@ int main() {
 
     cout << "Scan saved to: " << fullPath << endl;
 
-    return 0;
+    return;
+    //ngl this function prolly leaks memory
 }
 
 
@@ -92,7 +131,7 @@ bool isExistingDir(const filesystem::path& p) noexcept
 
 
 
-bool CreateDirectoryRecursive(const std::string & dirName)
+bool CreateDirectoryRecursive(const string & dirName)
 {
     error_code err;
     if (!filesystem::create_directories(dirName, err))
@@ -107,4 +146,47 @@ bool CreateDirectoryRecursive(const std::string & dirName)
     }
     return true;
 }
+
+
+void timer_start(function<void(void)> func, unsigned int interval)
+{
+    thread([func, interval]() {
+        while (true)
+        {
+            func();
+            this_thread::sleep_for(chrono::milliseconds(interval));
+        }
+    }).detach();
+}
+
+
+Coordinates parseCoordinates(const string& inputString) {
+    Coordinates coords;
+
+    stringstream ss(inputString);
+    ss >> coords.latitude;
+    ss.ignore();  // Ignore the comma separator
+    ss >> coords.longitude;
+
+    return coords;
+}
+
+
+double calculateDistance(double lat1, double lon1, double lat2, double lon2)
+{
+    double earthRadius = 20902231.98757;  // Earth's radius in feet
+    double dLat = (lat2 - lat1) * M_PI / 180.0;  // Latitude difference in radians
+    double dLon = (lon2 - lon1) * M_PI / 180.0;  // Longitude difference in radians
+
+    // Haversine formula to calculate distance
+    double a = sin(dLat / 2) * sin(dLat / 2) +
+               cos(lat1 * M_PI / 180.0) * cos(lat2 * M_PI / 180.0) *
+               sin(dLon / 2) * sin(dLon / 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    double distance = earthRadius * c;
+
+    return distance;
+}
+
+
 
